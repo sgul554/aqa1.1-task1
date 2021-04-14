5e77de277eb20f2cdae6915763e081d82f3385f0
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-04-14 19:54:17 +0300

    Добавление кода и тестов testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,20 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void shouldCheckRemain() {
+        assertEquals(cashbackHackService.remain(900), 100);
+    }
+
+    @Test
+    public void shouldCheckRemainIfAmountEqualToBoundary() {
+        assertEquals(cashbackHackService.remain(1000), 0);
+
+    }
+}
