5c295d56abac56de9d155234bfbb26fc0829b440
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-04-14 20:31:12 +0300

    Добавление кода и тестов junit4

---
+++ src/test/java/ru/netology/service/CashbackHackerTest.java
@@ -0,0 +1,20 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackerTest {
+    @Test
+    public void shouldCheckRemain() {
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        assertEquals(100, cashbackHacker.remain(900));
+    }
+    @Test
+    public void shouldCheckRemainIfAmountEqualToBoundary() {
+        CashbackHacker cashbackHacker = new CashbackHacker();
+        assertEquals(0, cashbackHacker.remain(1000));
+    }
+
+
+}
