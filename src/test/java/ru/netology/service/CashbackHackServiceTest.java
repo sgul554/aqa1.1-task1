46b9b81aedd006395b05db8f4a44c45748bc3139
Sasha Gulyaeva <sgulayeva@gmail.com>
2021-11-27 16:41:11 +0300

    test success

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -9,12 +9,13 @@ public class CashbackHackServiceTest {
 
     @Test
     public void shouldCheckRemain() {
+
         assertEquals(cashbackHackService.remain(900), 100);
     }
 
     @Test
     public void shouldCheckRemainIfAmountEqualToBoundary() {
-        assertEquals(cashbackHackService.remain(1000), 0);
+        assertEquals(cashbackHackService.remain(1000), 1000);
 
     }
 }
